214b6769c204e3eeb0b6c0fb9d0b92b10ec7ec44
fitriq751-max <fitriq751@gmail.com>
2026-03-13 11:35:25 +0700

    Update book.py

--- book.py
+++ book.py
@@ -1,7 +1,19 @@
+import datetime
+
 class Book:
-    def __init__(self, id_buku, judul, penulis, tahun):
+    def __init__(self, id_buku,judul, penulis, tahun):
         self.__id_buku = id_buku
         self.judul = judul
         self.penulis = penulis
         self.tahun = tahun
         self.status = "tersedia"
+        self.__update_terakhir = datetime.datetime.now()
+    
+    def update_judul (self,judul):
+        self.judul = judul
+        self.__update_terakhir = datetime.datetime.now()
+
+    def get_update_terakhir (self):
+        return self.__update_terakhir
+
+    
